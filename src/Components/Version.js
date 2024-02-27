1221d91bb4ee44f0829476b38f644df5b3095f7f
Tran Dinh Hung <hungtd@iMacPro.local>
2024-02-27 16:33:17 +0700

    begin upload

--- src/Components/Version.js
+++ src/Components/Version.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function Version() {
   return (
-    <div style={{ color: "blue", display: "flex", marginLeft: 10 }}>
-      <h1> Version: 1.0</h1>
+    <div>
+      <div style={{ color: "blue", display: "flex", marginLeft: 10 }}>
+        <h1> Version: 1.0</h1>
+      </div>
+      <div style={{ display: "flex", marginLeft: 10 }}>
+        <h2>Date Change: 27/02/2024</h2>
+      </div>
     </div>
   );
 }
